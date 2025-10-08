355dbde1a730fdd1958353baf265429f663ffa9f
Husky Robotics <uwrobots@uw.edu>
2025-10-08 15:33:22 -0700

    CMake, argparse, gitignore

--- src/Rover.cpp
+++ src/Rover.cpp
@@ -1,5 +1,22 @@
-#include <cstdio>
+#include <iostream>
+
+#include <argparse/argparse.hpp>
 
 int main(int argc, char** argv) {
-  std::out << "Hello";
+  argparse::ArgumentParser program("Rover");
+
+  std::string peripheral;
+
+  program.add_argument("-p", "--peripheral")
+    .help("specifies which peripheral is mounted")
+    .default_value("null")
+    .store_into(peripheral);
+
+  try {
+    program.parse_args(argc, argv);
+  } catch (const std::exception& err) {
+    std::cerr << err.what() << std::endl;
+    std::cerr << program;
+    std::exit(1);
+  }
 }
