512719542a6f0b6cc8808ac9f68de0d82d83496d
Isaac <isaacw1925@gmail.com>
2026-01-21 11:27:34 -0800

    Test namespaces

--- src/Constants.cpp
+++ src/Constants.cpp
@@ -1,10 +1,36 @@
+/**
+  @namespace Constants
+  @brief Constants
+*/
 namespace Constants {
 
+/**
+  @namespace Network
+  @brief Network constants
+*/
 namespace Network {
 const char* MC_PROTOCOL_ENDPOINT = "/mission-control";
+
+/**
+  @namespace Keys
+  @brief Network keys
+*/
+namespace Keys {
+constexpr const char* PERIPHERAL_REPORT = "peripheralReport";
+
+} // namespace Keys
+
+} // namespace Network
+
+/**
+  @namespace Constants::Drive
+  @brief Drive constants
+*/
+namespace Drive {
+const double ROBOT_LENGTH = 1.0;
 }
 
-}
+} // namespace Constants
 
 // Gamepad Ports
 // CAN IDs
