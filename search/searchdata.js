8ae1165a047d5dda024ec6e27bd63807f59e5716
imisaacwu <imisaacwu@users.noreply.github.com>
2026-02-12 19:54:51 +0000

    deploy: cbca62030a3e57e04cc8431f1ab8c13ecbcb8600

--- search/searchdata.js
+++ search/searchdata.js
@@ -1,12 +1,21 @@
 var indexSectionsWithContent =
 {
+  0: "cr",
+  1: "r",
+  2: "c"
 };
 
 var indexSectionNames =
 {
+  0: "all",
+  1: "classes",
+  2: "namespaces"
 };
 
 var indexSectionLabels =
 {
+  0: "All",
+  1: "Classes",
+  2: "Namespaces"
 };
 
